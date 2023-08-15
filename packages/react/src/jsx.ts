feb67f648a65e1fbfc65d6165829d84528ce6803
yanhengfu <1315007322@qq.com>
2023-08-15 08:01:52 +0800

    feat: add jsx fn

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -1,14 +1,21 @@
 /*
  * @Author: yanhengfu yanhengfu@jimengio.com
  * @Date: 2023-08-11 14:51:31
- * @LastEditors: yanhengfu yanhengfu@jimengio.com
- * @LastEditTime: 2023-08-11 18:19:17
+ * @LastEditors: yanhengfu 1315007322@qq.com
+ * @LastEditTime: 2023-08-15 08:01:21
  * @FilePath: \yhf_react\packages\react\src\jsx.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
 import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
-import { Key, Props, Ref, Type, ReactElement } from 'shared/ReactTypes';
+import {
+	Key,
+	Props,
+	Ref,
+	Type,
+	ReactElement,
+	ElementType
+} from 'shared/ReactTypes';
 
 //RectElement 构造函数
 const ReactElement = (
@@ -27,3 +34,9 @@ const ReactElement = (
 	};
 	return Element;
 };
+
+export const jsx = (
+	type: ElementType,
+	config: any,
+	...maybeChildren: any
+) => {};
--- packages/shared/ReactTypes.ts
+++ packages/shared/ReactTypes.ts
@@ -10,6 +10,7 @@ export type Type = any;
 export type Key = any;
 export type Ref = any;
 export type Props = any;
+export type ElementType = any;
 
 export interface ReactElement {
 	$$typeof: symbol | number;
