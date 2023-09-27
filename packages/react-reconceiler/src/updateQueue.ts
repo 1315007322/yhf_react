f832eb31be3ddf904d235bee4f5153b6b10699de
yanhengfu <yanhengfu@jimengio.com>
2023-09-27 18:35:37 +0800

    feat: updataeQueue

---
+++ packages/react-reconceiler/src/updateQueue.ts
@@ -0,0 +1,10 @@
+import { Actions } from 'shared/ReactTypes';
+
+export interface Update<State> {
+	action: Actions<State>;
+}
+
+
+export const createUpdate = () => {
+  
+}
--- packages/shared/ReactTypes.ts
+++ packages/shared/ReactTypes.ts
@@ -2,7 +2,7 @@
  * @Author: yanhengfu yanhengfu@jimengio.com
  * @Date: 2023-08-11 17:52:32
  * @LastEditors: yanhengfu yanhengfu@jimengio.com
- * @LastEditTime: 2023-08-11 18:22:36
+ * @LastEditTime: 2023-09-25 18:05:30
  * @FilePath: \yhf_react\packages\shared\ReactTypes.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,3 +20,5 @@ export interface ReactElement {
 	props: Props;
 	_mark: string;
 }
+
+export type Actions<State> = State | ((preState: State) => State);
